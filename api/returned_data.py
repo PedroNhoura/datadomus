9b66b7bac29bb15093ba0ebc9b2df865b4f0465f
Pedro Moura <pedro.moura@skallardigital.com.br>
2023-12-07 13:28:30 -0300

    Dados em Cloud já transformados

--- api/returned_data.py
+++ api/returned_data.py
@@ -1,37 +1,46 @@
 import pandas as pd
-from main import FogoCruzadoAPI
+import geopandas as gpd
+from shapely.geometry import Point
+from connection import FogoCruzadoAPI
 
-def obter_dados_ocorrencias(api, take=20, id_state="b112ffbe-17b3-4ad0-8f2a-2038745d1d14"):
-    # Construindo a URL com os parâmetros desejados
-    url = f"https://api-service.fogocruzado.org.br/api/v2/occurrences?order=ASC&page=1&take={take}&idState={id_state}"
+class FogoCruzadoData:
+    def __init__(self, email, password):
+        self.api = FogoCruzadoAPI(email, password)
 
-    # Fazendo a solicitação para obter os dados de ocorrências
-    response = api.request_with_token(url)
+    def obter_dados_ocorrencias(self, take=20, id_state="b112ffbe-17b3-4ad0-8f2a-2038745d1d14"):
+        url = f"https://api-service.fogocruzado.org.br/api/v2/occurrences?order=ASC&page=1&take={take}&idState={id_state}"
+        response = self.api.request_with_token(url)
 
-    # Verificando se a solicitação foi bem-sucedida (status 200)
-    if response.status_code == 200:
-        # Convertendo a resposta JSON para um DataFrame pandas
-        data = response.json()['data']
-        tabela_dados = pd.DataFrame(data)
-        return tabela_dados
-    else:
-        print(f"A solicitação falhou com o status {response.status_code}")
-        return None
+        if response.status_code == 200:
+            data = response.json()['data']
+            tabela_dados = pd.DataFrame(data)
+            return tabela_dados
+        else:
+            print(f"A solicitação falhou com o status {response.status_code}")
+            return None
+
+    def criar_geodataframe(self, dados):
+        # Verifica se as colunas de latitude e longitude existem nos dados
+        if 'latitude' in dados.columns and 'longitude' in dados.columns:
+            # Cria a geometria Point usando as colunas de latitude e longitude
+            geometry = [Point(float(lon), float(lat)) for lat, lon in zip(dados['latitude'], dados['longitude'])]
+            
+            # Cria o GeoDataFrame
+            gdf = gpd.GeoDataFrame(dados, geometry=geometry, crs="EPSG:4326")
+            return gdf
+        else:
+            print("As colunas de latitude e/ou longitude não foram encontradas nos dados.")
+            return None
 
 # Uso
 email = "crixusalter@gmail.com"
 password = "DDpedro@123"
-api = FogoCruzadoAPI(email, password)
 
-# Obtendo os dados de ocorrências
-dados_ocorrencias = obter_dados_ocorrencias(api, take=500)
 
-# Verificando se os dados foram obtidos com sucesso
-# if dados_ocorrencias is not None:
-#     # Imprimindo informações e a primeira linha do DataFrame
+fogo_cruzado_data = FogoCruzadoData(email, password)
+
+# # Obtendo os dados de ocorrências
+dados_ocorrencias = fogo_cruzado_data.obter_dados_ocorrencias(take=2000)
+
 print(dados_ocorrencias.info())
-print(dados_ocorrencias.address.iloc[0])  # Exemplo de impressão de uma coluna específica (address)
 
-
-
-# achar o index do json onde estao as coordenadas para cria um GEODATAFRAME 
