19c2277f9e629370a8b94ba628ab3e41cf804091
Pedro Moura <pedro.moura@skallardigital.com.br>
2024-01-08 17:21:36 -0300

    Adicionado ponto barra de progresso e excluido bd local docker

--- api/insert_data_bigquery.py
+++ api/insert_data_bigquery.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from returned_data_fogocruzado import FogoCruzadoData
 from google.cloud import bigquery
+from tqdm import tqdm
 
 class LoadingToBigQuery:
     def __init__(self, project_id, dataset_id, table_id, credentials_path):
@@ -24,7 +25,10 @@ class LoadingToBigQuery:
         inserted_ids = []
         duplicate_inserts = []
 
-        for i in range(0, len(dataframe), batch_size):
+        total_batches = len(dataframe) // batch_size + (1 if len(dataframe) % batch_size > 0 else 0)
+        tqdm_batch_iterator = tqdm(range(0, len(dataframe), batch_size), total=total_batches, desc="Progresso")
+
+        for i in tqdm_batch_iterator:
             batch_df = dataframe.iloc[i:i + batch_size]
 
             for _, row in batch_df.iterrows():
@@ -37,16 +41,11 @@ class LoadingToBigQuery:
                     job = self.client.load_table_from_dataframe(pd.DataFrame([row]), table_ref, job_config=job_config)
                     job.result()
 
-        print(f'IDs duplicados: {len(duplicate_inserts)}')
-        print(f'IDs inseridos: {len(inserted_ids)}')
+            tqdm_batch_iterator.set_postfix({"IDs duplicados": len(duplicate_inserts), "IDs inseridos": len(inserted_ids)})
+
+        print(f'Total de resultados consultados: {len(dataframe)}')
 
 # Uso
-# ... (restante do código)
-
-# ... (restante do código)
-
-# ... (restante do código)
-
 if __name__ == "__main__":
     project_id = 'encoded-density-405819'
     dataset_id = 'api_fogo_cruzado'
@@ -56,8 +55,8 @@ if __name__ == "__main__":
     fogo_cruzado_data = FogoCruzadoData("crixusalter@gmail.com", "DDpedro@123")
     
     # Defina as datas desejadas
-    initial_date = "2023-01-01"
-    final_date = "2023-01-17"
+    initial_date = "2023-01-25"
+    final_date = "2023-01-30"
 
     dados_ocorrencias = fogo_cruzado_data.obter_dados_ocorrencias(
         id_state="b112ffbe-17b3-4ad0-8f2a-2038745d1d14",
@@ -71,4 +70,3 @@ if __name__ == "__main__":
     loader = LoadingToBigQuery(project_id, dataset_id, table_id, credentials_path)
     loader.load_dataframe_to_bigquery(dados_ocorrencias_filtrado)
     print(f'Total de resultados consultados: {len(dados_ocorrencias)}')
-
